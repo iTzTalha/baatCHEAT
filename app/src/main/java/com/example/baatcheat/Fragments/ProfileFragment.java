cc586893eff83fe2a1c3223147b7fa760ab2e940
DrXp3rT0 <shaikhtalha6604@gmail.com>
2019-10-31 01:47:15 +0530

    Commit 12: Populated username in profile and username is changeable now

--- app/src/main/java/com/example/baatcheat/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/baatcheat/Fragments/ProfileFragment.java
@@ -17,21 +17,34 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.baatcheat.LoginActivity;
+import com.example.baatcheat.Model.User;
 import com.example.baatcheat.R;
 import com.example.baatcheat.StartActivity;
+import com.example.baatcheat.UsernameActivity;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ProfileFragment extends Fragment {
 
+    private LinearLayout Username;
     private LinearLayout logout;
+
+    private TextView fixedText,Editusername;
 
     private FirebaseAuth mAuth;
 
+    FirebaseUser firebaseUser;
     public ProfileFragment() {
         // Required empty public constructor
     }
@@ -42,9 +55,25 @@ public class ProfileFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
+        Username = view.findViewById(R.id.username);
         logout = view.findViewById(R.id.logout);
 
+        fixedText = view.findViewById(R.id.fixedText);
+        fixedText.setText("@");
+        Editusername = view.findViewById(R.id.Editusername);
+
         mAuth = FirebaseAuth.getInstance();
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        updateProfileInfo();
+
+        Username.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), UsernameActivity.class);
+                startActivity(intent);
+            }
+        });
 
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,6 +88,26 @@ public class ProfileFragment extends Fragment {
         });
 
         return  view;
+    }
+
+    private void updateProfileInfo() {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                User users = dataSnapshot.getValue(User.class);
+                Editusername.setText(users.getUsername());
+//                editBio.setText(users.getBio());
+//                Glide.with(getApplicationContext()).load(users.getImageurl()).into(imageView);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
 }
---
+++ app/src/main/java/com/example/baatcheat/UsernameActivity.java
@@ -0,0 +1,134 @@
+package com.example.baatcheat;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.StyleSpan;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.andexert.library.RippleView;
+import com.example.baatcheat.Model.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+
+public class UsernameActivity extends AppCompatActivity {
+
+    private EditText username;
+    private TextView text1,text2;
+    ImageView goBack,done;
+
+    FirebaseUser firebaseUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_username);
+
+        username = findViewById(R.id.username);
+        text1 = findViewById(R.id.text1);
+        text2 = findViewById(R.id.text2);
+        goBack = findViewById(R.id.backToMain);
+        done = findViewById(R.id.done);
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        updateEditTextAtStart();
+        spannableBoldText();
+
+        done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String str_username = username.getText().toString().replace(" ","").trim();
+
+                Query usernameQuery = FirebaseDatabase.getInstance().getReference("Users").orderByChild("username").equalTo(str_username);
+                usernameQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                        if (dataSnapshot.exists()){
+                            username.setError("Username is taken");
+                            username.requestFocus();
+                            return;
+                        }else if (username.length() < 5){
+                            username.setError("Username must be between 5 - 25 characters");
+                            username.requestFocus();
+                            return;
+                        } else {
+                            updateProfile(username.getText().toString().replace(" ","").trim());
+                            finish();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+
+        goBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    private void spannableBoldText(){
+        String str_text2 = ("Name must be between 5-25 characters.");
+        SpannableString spannableString1 = new SpannableString(str_text2);
+        StyleSpan boldSpan1 = new StyleSpan(Typeface.BOLD);
+        spannableString1.setSpan(boldSpan1, 21, 25, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        text2.setText(spannableString1);
+
+        String str_text1 = ("You can use a-z,A-Z,0-9 and underscores.");
+        SpannableString spannableString2 = new SpannableString(str_text1);
+        StyleSpan boldSpan2 = new StyleSpan(Typeface.BOLD);
+        spannableString2.setSpan(boldSpan2, 12, 23, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        text1.setText(spannableString2);
+    }
+
+    private void updateEditTextAtStart(){
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                User users = dataSnapshot.getValue(User.class);
+                username.setText(users.getUsername());
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void updateProfile(String username) {
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
+
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("username", username);
+
+        reference.updateChildren(hashMap);
+    }
+}
