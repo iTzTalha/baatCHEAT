3b9a7fcebdf3a68e3f4eb1667b3cf7cfaa6da4e0
DrXp3rT0 <shaikhtalha6604@gmail.com>
2019-10-30 19:39:29 +0530

    Commit 11: Created Profile Fragment...

--- app/src/main/java/com/example/baatcheat/Adapter/UserAdapter.java
+++ app/src/main/java/com/example/baatcheat/Adapter/UserAdapter.java
@@ -1,10 +1,13 @@
 package com.example.baatcheat.Adapter;
 
+import android.app.Dialog;
 import android.content.Context;
-import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -23,6 +26,7 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
 
     private Context mContext;
     private List<User> mUsers;
+    private Dialog mDialog;
 
     FirebaseUser firebaseUser;
 
@@ -36,10 +40,17 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
         View view = LayoutInflater.from(mContext).inflate(R.layout.item_user, parent, false);
-        RecyclerView.LayoutParams layoutParams = new RecyclerView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        RecyclerView.LayoutParams layoutParams = new RecyclerView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         view.setLayoutParams(layoutParams);
 
-        return new UserAdapter.ViewHolder(view);
+        ViewHolder mviewHolder = new ViewHolder(view);
+
+        //dialog ini
+        mDialog = new Dialog(mContext);
+        mDialog.setContentView(R.layout.dialog_contact);
+        mDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        return mviewHolder;
     }
 
     @Override
@@ -54,6 +65,19 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
         holder.phoneNumber.setText(users.getPhone());
 //        Glide.with(mContext).load(users.getImageurl()).into(holder.image_Profile);
 
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView dialog_username = mDialog.findViewById(R.id.dialog_username);
+                TextView dialog_phone = mDialog.findViewById(R.id.dialog_phone);
+                ImageView dialog_imageProfile = mDialog.findViewById(R.id.dialog_ImageProfile);
+
+                dialog_username.setText(users.getUsername());
+                dialog_phone.setText(users.getPhone());
+                mDialog.show();
+            }
+        });
+
     }
 
     @Override
@@ -63,7 +87,7 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
-        public TextView username,phoneNumber;
+        public TextView username, phoneNumber;
         public CircleImageView image_Profile;
 
         public ViewHolder(@NonNull View itemView) {
--- app/src/main/java/com/example/baatcheat/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/baatcheat/Fragments/ProfileFragment.java
@@ -1,6 +1,7 @@
 package com.example.baatcheat.Fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,13 +9,21 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
+import com.example.baatcheat.LoginActivity;
 import com.example.baatcheat.R;
+import com.example.baatcheat.StartActivity;
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ProfileFragment extends Fragment {
+
+    private LinearLayout logout;
+
+    private FirebaseAuth mAuth;
 
 
     public ProfileFragment() {
@@ -26,7 +35,25 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        logout = view.findViewById(R.id.logout);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                getActivity().finish();
+            }
+        });
+
+        return  view;
     }
 
 }
