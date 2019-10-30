55e29f7f5c22c6a2fdfd6502806da97856b72233
DrXp3rT0 <shaikhtalha6604@gmail.com>
2019-10-30 21:29:28 +0530

    Commit 11: Created Profile Fragment...

--- app/src/main/java/com/example/baatcheat/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/baatcheat/Fragments/ProfileFragment.java
@@ -4,11 +4,18 @@ package com.example.baatcheat.Fragments;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.example.baatcheat.LoginActivity;
@@ -25,11 +32,9 @@ public class ProfileFragment extends Fragment {
 
     private FirebaseAuth mAuth;
 
-
     public ProfileFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
